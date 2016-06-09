7b08f392929562035ae30222536563403b79ae92
Thom Allen <thomwork@gmail.com>
2016-06-09 23:21:02 +0100

    Remove comments from CSS module - DIDNT LIKE IT
    +expand cssmod example

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,18 @@ import styles from './App.scss';
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.root}>
         <h1>Hello world, from React</h1>
         <p className={styles.red}>red</p>
         <p className={styles.green}>green</p>
         <p className={styles.blue}>blue</p>
+        <div className={styles.some}>
+          <div className={styles.nested}>
+            <div className={styles.shite}>
+              <p>ey!</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
